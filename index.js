16b9e9a06f32a1090bb8dd36d656f7d76202943b
Jonas Almeida <jalmeida@mathbiol.org>
2018-11-09 14:02:21 -0500

    header on

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8080
 console.log('index.js loaded, port = '+port)
 
 http.createServer(function (request, response) {
-   //response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.writeHead(200, {'Content-Type': 'text/plain'});
    if(request.url!="/favicon.ico"){
        response.end('Hello World at '+Date());
    }else{
