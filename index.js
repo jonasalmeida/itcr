e73cd663bb535db523ec3bc96b5de12d84c4b4b2
Jonas Almeida <jalmeida@mathbiol.org>
2018-11-09 12:45:32 -0500

    favicon filter

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-console.log('index.js loaded')
+
 var http=require('http')
 var port = process.env.PORT || 8080
-
+console.log('index.js loaded, port = '+port)
 
 http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello World\n');
+   //response.writeHead(200, {'Content-Type': 'text/plain'});
+   if(request.url!="/favicon.ico"){
+       response.end('Hello World at '+Date());
+   }else{
+       response.end()
+   }
+   
 }).listen(port);
 
 // Console will print the message
