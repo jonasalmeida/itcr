19a37d0167071f045a2b1a1ab66fa4e5f8c775e7
Jonas Almeida <jalmeida@mathbiol.org>
2018-11-09 14:07:57 -0500

    headless

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ console.log('index.js loaded, port = '+port)
 
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   if(request.url!="/favicon.ico"){
+   //if(request.url!="/favicon.ico"){
        response.end('Hello World at '+Date());
-   }else{
-       response.end()
-   }
+   //}else{
+   //    response.end()
+   //}
    
 }).listen(port);
 
